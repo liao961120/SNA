8990684a740eb631b4500ce79440a23ebd6069ae
liao <liaomovie2@gmail.com>
2019-04-29 16:07:59 +0800

    up

---
+++ build_idx.R
@@ -0,0 +1,12 @@
+setwd('~/Desktop/SNA')
+
+files <- list.files(pattern='html$', recursive = T, full.names = T)
+file_ext <- function(...) tools::file_ext(...)
+
+
+
+out_str <- paste0('<a target="_blank" href="', files, '">', files, '</a><br>')
+
+out_str <- c('', out_str)
+
+writeLines(out_str, con = 'index.html')
