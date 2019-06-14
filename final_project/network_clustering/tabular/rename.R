1aaaea97a1bc3c033ee4604e4fdf8eef22cf6040
liao <liao961120@gmail.com>
2019-06-14 20:50:19 +0800

    rename transfer network

--- final_project/network_clustering/tabular/rename.R
+++ final_project/network_clustering/tabular/rename.R
@@ -1,9 +1,10 @@
 library(dplyr)
 lookup <- readr::read_csv('name_lookup.csv')
-x = readr::read_csv('transfer_netSEPart.csv')
+x2 = readr::read_csv('../raw_network_data/transfer_edges.csv')
 
-for (i in seq_along(x$X1)) {
-  x$X1[i] = lookup$college104[lookup$transfer == x$X1[i]]
+for (i in seq_along(x$source)) {
+  x$source[i] = lookup$college104[lookup$transfer == x$source[i]]
+  x$target[i] = lookup$college104[lookup$transfer == x$target[i]]
 }
 
-readr::write_csv(x, '../transfer_netSEPart2.csv')
+readr::write_csv(x, '../raw_network_data/transfer_edges2.csv')
