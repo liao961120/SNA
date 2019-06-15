1f3127b1117c8ac312dc5b366a835c29705a1ee6
liao <liao961120@gmail.com>
2019-06-15 09:49:19 +0800

    merge 104ntu application network attribute data

---
+++ final_project/join_network_attributes.R
@@ -0,0 +1,22 @@
+library(dplyr)
+ntu_attr <- readr::read_csv('ntuApplic_attributes.csv')
+transfer_attr <- readr::read_csv('transfer_attributes.csv')
+
+ntu_attr <- ntu_attr %>% 
+  rename(Id = Label) %>%
+  rename(ntuApplic_pageranks = pageranks) %>%
+  rename(ntuApplic_degree = Degree) %>%
+  rename(ntuApplic_clustering_coefficient = clustering) %>%
+  rename(ntuApplic_eigenCentrality = eigencentrality) %>%
+  select(-college)
+
+transfer_attr <- transfer_attr %>%
+  rename(transfer_indegree = indegree) %>%
+  rename(transfer_outdegree = outdegree) %>%
+  rename(transfer_degree = Degree) %>%
+  rename(transfer_eigenCentrality = eigencentrality)
+
+
+x = left_join(transfer_attr, ntu_attr, by = c('Id' = 'Id'), keep = T)
+
+readr::write_csv(x, 'network_attributes.csv')
